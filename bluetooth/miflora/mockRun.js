bab71099b4852c37cb7bdab5fe0a1718520c0059
Josh McArthur <joshua.mcarthur@gmail.com>
2019-04-06 16:20:00 +1300

    Replace custom Node.js implementation with Python library

--- bluetooth/miflora/mockRun.js
+++
@@ -1,5 +0,0 @@
-const run = function() {
-  return new Promise(resolve => setTimeout(resolve, 10000));
-}
-
-module.exports = run;
--- bluetooth/miflora/run.js
+++
@@ -1,8 +0,0 @@
-const miflora = require("miflora");
-const run = async function() {
-  const devices = await miflora.discover();
-  return Promise.all(devices.map(d => d.querySensorValues())) 
-}
-
-module.exports = run;
-
